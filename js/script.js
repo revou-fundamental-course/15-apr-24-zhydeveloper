a7ccc4a277ec662995f967c010813646087eb0da
ZHYY DEVOLOPER <azhysholihin@hotmail.com>
2024-04-21 21:45:54 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
- // Function to convert Celsius to Fahrenheit
- function celsiusToFahrenheit(celsius) {
+// Function to convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
   return celsius * 9/5 + 32;
 }
 
@@ -14,23 +14,58 @@ function resetInputs() {
   document.getElementById('inptTextFar').value = '';
   document.getElementById('inptTextCalculation').value = '';
 }
+// popup
+function showPopup(message) {
+  var popup = document.createElement('div');
+  popup.className = 'popup-overlay';
+  
+  var popupContent = document.createElement('div');
+  popupContent.className = 'popup-content';
+  
+  var popupMessage = document.createElement('p');
+  popupMessage.textContent = message;
+  
+  var popupButton = document.createElement('button');
+  popupButton.className = 'popup-btn';
+  popupButton.textContent = 'Close';
+  popupButton.onclick = hidePopup;
+  
+  popupContent.appendChild(popupMessage);
+  popupContent.appendChild(popupButton);
+  popup.appendChild(popupContent);
+  
+  document.body.appendChild(popup);
+}
 
-// Function to reverse the conversion from Celsius to Fahrenheit
-function reverseCelsiusToFahrenheit() {
-  var celInput = document.getElementById('inptTextCel').value;
-  if (celInput !== '') {
-      var farValue = celsiusToFahrenheit(parseFloat(celInput));
-      document.getElementById('inptTextFar').value = farValue;
+function hidePopup() {
+  var popup = document.querySelector('.popup-overlay');
+  if (popup) {
+    popup.parentNode.removeChild(popup);
   }
 }
 
-// Function to reverse the conversion from Fahrenheit to Celsius
+
+// Function to reverse the conversion from Celsius to Fahrenheit
+function reverseCelsiusToFahrenheit() {
+    var celInput = document.getElementById('inptTextCel').value;
+    if (celInput.trim() === '') {
+        showPopup('Mohon isi input dengan nilai suhu dalam Celcius!');
+        return;
+    }
+    
+    var farValue = celsiusToFahrenheit(parseFloat(celInput));
+    document.getElementById('inptTextFar').value = farValue;
+}
+
 function reverseFahrenheitToCelsius() {
-  var farInput = document.getElementById('inptTextFar').value;
-  if (farInput !== '') {
-      var celValue = fahrenheitToCelsius(parseFloat(farInput));
-      document.getElementById('inptTextCel').value = celValue;
-  }
+    var farInput = document.getElementById('inptTextFar').value;
+    if (farInput.trim() === '') {
+        showPopup('Mohon isi input dengan nilai suhu dalam Fahrenheit!');
+        return;
+    }
+    
+    var celValue = fahrenheitToCelsius(parseFloat(farInput));
+    document.getElementById('inptTextCel').value = celValue;
 }
 
 // Function to display calculation formula
